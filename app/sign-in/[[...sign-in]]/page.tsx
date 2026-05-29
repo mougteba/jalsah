8546fb853ac62966063fc0240291d66ef7a27309
mougtabe <almAlmjtbymhmdalmkhtar@gmail.com>
2026-05-29 13:41:41 +0000

    إصلاح sign-in pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50" dir="rtl">
-      <SignIn afterSignInUrl="/redirect" afterSignUpUrl="/redirect" />
+      <SignIn />
     </div>
   );
 }
