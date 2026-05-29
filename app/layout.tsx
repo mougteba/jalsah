a2c48c3658a09062de1bbb5e73d0006e336b008d
mougtabe <almAlmjtbymhmdalmkhtar@gmail.com>
2026-05-29 14:48:52 +0000

    fix: use env var for clerk after sign in redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignOutUrl="/" afterSignInUrl="/redirect">
+    <ClerkProvider afterSignOutUrl="/">
       <html lang="ar" dir="rtl">
         <body>
           <ConvexClientProvider>
