0afe24b447bb60af11122be987f86fc90017a4ff
mougtabe <almAlmjtbymhmdalmkhtar@gmail.com>
2026-05-29 14:29:26 +0000

    fix: add clerk middleware and afterSignInUrl redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider afterSignOutUrl="/" afterSignInUrl="/redirect">
       <html lang="ar" dir="rtl">
         <body>
           <ConvexClientProvider>
